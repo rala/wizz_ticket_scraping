8d2c322efca6ef1b29b9bc0360095a490ea4a0bd
wei liao <wei.liao@laerdal.com>
2022-07-01 13:57:36 +0200

    move workdays out of main file

--- wizz.py
+++ wizz.py
@@ -6,6 +6,7 @@ import requests
 import json 
 import copy
 import codecs
+from workdays import *
 
 #payload={"flightList":[{"departureStation":"SVG","arrivalStation":"GDN","from":"2022-07-01","to":"2022-08-01"},{"departureStation":"GDN","arrivalStation":"SVG","from":"2022-08-01","to":"2022-09-01"}],"priceType":"wdc","adultCount":1,"childCount":0,"infantCount":0}
 # payload={'flightList':[{"departureStation": "SVG", "arrivalStation": "GDN", "from": "2022-07-01", "to": "2022-08-01"}], 'priceType': 'wdc', 'adultCount': 1, 'childCount': 0, 'infantCount': 0}
@@ -19,56 +20,7 @@ travel_list = []
 min_travel_days = 3
 max_travel_days = 7
 max_price = 800
-class workDays():
-    def __init__(self, start_date, end_date, days_off=None):
-        """days_off:休息日,默认周六日, 以0(星期一)开始,到6(星期天)结束, 传入tupple
-        没有包含法定节假日,
-        """
-        self.start_date = start_date
-        self.end_date = end_date
-        self.days_off = days_off
-        if self.start_date > self.end_date:
-            self.start_date,self.end_date = self.end_date, self.start_date
-        if days_off is None:
-            self.days_off = 5,6
-        # 每周工作日列表
-        self.days_work = [x for x in range(7) if x not in self.days_off]
- 
-    def workDays(self):
-        """实现工作日的 iter, 从start_date 到 end_date , 如果在工作日内,yield 日期
-        """
-        # 还没排除法定节假日
-        tag_date = self.start_date
-        while True:
-            if tag_date > self.end_date:
-                break
-            if tag_date.weekday() in self.days_work:
-                yield tag_date
-            tag_date += datetime.timedelta(days=1)
- 
-    def daysCount(self):
-        """工作日统计,返回数字"""
-        return len(list(self.workDays()))
- 
-    def weeksCount(self, day_start=0):
-        """统计所有跨越的周数,返回数字
-        默认周从星期一开始计算
-        """
-        day_nextweek = self.start_date
-        while True:
-            if day_nextweek.weekday() == day_start:
-                break
-            day_nextweek += datetime.timedelta(days=1)
-        # 区间在一周内
-        if day_nextweek > self.end_date:
-            return 1
-        weeks = ((self.end_date - day_nextweek).days + 1)/7
-        weeks = int(weeks)
-        if ((self.end_date - day_nextweek).days + 1)%7:
-            weeks += 1
-        if self.start_date < day_nextweek:
-            weeks += 1
-        return weeks
+
 class Travel:
     def __init__(self, out_bound_flight, in_bound_flight, price, days, work_days):
         self.out_bound_flight = out_bound_flight
@@ -185,7 +137,7 @@ def Iterate_flights(out_flights_list,in_flights_list):
 def travel_filter(in_flight, out_flight):
 
     travel_days=(in_flight.date.date() - out_flight.date.date()).days + 1
-    work_day = workDays(out_flight.date.date(),in_flight.date.date())   
+    work_day = WorkDays(out_flight.date.date(),in_flight.date.date())   
     if travel_days>= min_travel_days and travel_days <= max_travel_days and in_flight.price+out_flight.price <= max_price and travel_days > work_day.daysCount():
         #generate a travel
         travel = Travel(out_flight,in_flight,in_flight.price+out_flight.price,travel_days, work_day.daysCount())
